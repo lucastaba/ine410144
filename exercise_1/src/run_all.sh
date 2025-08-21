348a8a5d0432faa93d8d2fd1e98f67a1f4014798
ltaba <lucas.taba@dma.ind.br>
2025-08-21 02:40:57 -0300

    add plots to solution

--- exercise_1/src/run_all.sh
+++ exercise_1/src/run_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run python script many times
-for file in $@; do python3 $1 -ng -s 0.1 -y vd_double_track/x_vec/v_x_mps vd_double_track/x_dot_vec/v_x_mps driver_input/acceleration -- $file; done
+for file in $@; do python3 $1 -ng -s 0.1 -y vd_double_track/x_vec/v_x_mps vd_double_track/x_dot_vec/v_x_mps driver_input/acceleration vd_double_track/x_dot_vec/v_z_mps -- $file; done
